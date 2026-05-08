33ed7a5f14953982f610a1fcf8a5e9f7a9578c6c
andrew6005 <andrew06052015@gmail.com>
2026-05-08 12:51:05 +0800

    2026-05-08-morming

---
+++ demo-call-api-fixed/src/main/java/com/bootcampspring/demo_call_api/mapper/DtoMapper.java
@@ -0,0 +1,11 @@
+package main.java.com.bootcampspring.demo_call_api.mapper;
+
+import com.bootcampspring.demo_call_api.dto.ForumUserDto;
+import com.bootcampspring.demo_call_api.entity.UserEntity;
+
+@Component
+public class DtoMapper {
+  public ForumUserDto map(UserEntity userEntity){
+    
+  }
+}
