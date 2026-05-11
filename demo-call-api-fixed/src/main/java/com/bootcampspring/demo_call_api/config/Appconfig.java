77a408d787ffe76ef1ab6211ad53e8f5f499880d
andrew6005 <andrew06052015@gmail.com>
2026-05-11 12:57:22 +0800

    2026-05-11

--- demo-call-api-fixed/src/main/java/com/bootcampspring/demo_call_api/config/Appconfig.java
+++ demo-call-api-fixed/src/main/java/com/bootcampspring/demo_call_api/config/Appconfig.java
@@ -3,7 +3,7 @@ package main.java.com.bootcampspring.demo_call_api.config;
 import java.beans.BeanProperty;
 
 @Configuration
-
+//! Configuration -> built in java ->Bean
 public class Appconfig {
   @Bean
   RestTemplate restTemplate;{
